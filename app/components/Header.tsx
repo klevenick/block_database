2e67ea41d1510032b259c0879f1c301814f33930
Kyle Levenick <Klevenick@gmail.com>
2025-07-18 15:38:57 -0400

    adding header, some style cleanup

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function Header() {
+
+    return (
+        <main className="header">
+            <h1><Link href="/">Block Mapper</Link></h1>
+        </main>
+    )
+}
